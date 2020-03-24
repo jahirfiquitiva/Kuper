caf2715a726678091a252475bc3cd753c89f7650
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-03-24 10:12:05 -0500

    Update Kuper version

--- buildSrc/src/main/java/Kuper.kt
+++ buildSrc/src/main/java/Kuper.kt
@@ -2,6 +2,6 @@
 
 object Kuper {
     const val appId = "dev.jahir.kuper.app"
-    const val version = 200
-    const val versionName = "2.0.0"
+    const val version = 201
+    const val versionName = "2.0.1"
 }
