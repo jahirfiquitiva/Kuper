777a2c014ab733d03996476171440bc641f221a6
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-04-07 21:13:04 -0500

    Update MyApp.kt

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "dev.jahir.kuper.app"
-    const val version = 207
-    const val versionName = "2.0.7"
+    const val version = 208
+    const val versionName = "2.0.8"
 }
