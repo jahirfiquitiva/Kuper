a102f6905e277ce1a49e8da528b0304b3d470ad7
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-04-11 18:27:01 -0500

    Update Versions.kt

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,7 +2,7 @@
 
 object Versions {
     // Plugins
-    const val gradle = "3.6.1"
+    const val gradle = "3.6.2"
     const val kotlin = "1.3.71"
     const val jitpack = "2.1"
 
@@ -16,5 +16,5 @@ object Versions {
     const val buildTools = "29.0.3"
 
     // Kuper
-    const val kuper = "2.1.0"
+    const val kuper = "2.1.1"
 }
