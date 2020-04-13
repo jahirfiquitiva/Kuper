0ceee97542dc2c98857afe5b8892993e05baf6cb
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-04-13 18:30:12 -0500

    Update Versions.kt

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -16,7 +16,7 @@ object Versions {
     const val buildTools = "29.0.3"
 
     // Frames
-    const val frames = "3.1.9"
+    const val frames = "7da1770"
 
     // Kustom API
     const val kustomApi = "6369c37"
