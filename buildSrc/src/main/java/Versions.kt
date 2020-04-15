db1e4dde872b129990156d0563a583c74c8c48ef
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-04-15 18:00:33 -0500

    Show title section in toolbar

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -16,7 +16,7 @@ object Versions {
     const val buildTools = "29.0.3"
 
     // Frames
-    const val frames = "5ef01ca"
+    const val frames = "103a3a2"
 
     // Kustom API
     const val kustomApi = "6369c37"
