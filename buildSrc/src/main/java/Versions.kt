7ea08672c7e419d37d9b310f9ba23692e7d3e0b2
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-05-17 18:02:53 -0500

    Update Versions.kt

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -7,7 +7,7 @@ object Versions {
     const val jitpack = "2.1"
 
     // OneSignal
-    const val oneSignalPlugin = "0.12.6"
+    const val oneSignalPlugin = "0.12.7"
     const val oneSignal = "3.13.2"
 
     // App
@@ -16,5 +16,5 @@ object Versions {
     const val buildTools = "29.0.3"
 
     // Kuper
-    const val kuper = "2.1.6"
+    const val kuper = "2.1.9"
 }
