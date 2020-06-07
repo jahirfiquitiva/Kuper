d6dd55fd7c264f7cbb0722c9dad82bdb66fcfe9a
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-06-07 16:24:56 -0500

    Implement Muzei API

---
+++ library/src/main/kotlin/dev/jahir/kuper/ui/activities/KuperMuzeiSettingsActivity.kt
@@ -0,0 +1,7 @@
+package dev.jahir.kuper.ui.activities
+
+import dev.jahir.frames.ui.activities.MuzeiSettingsActivity
+
+class KuperMuzeiSettingsActivity : MuzeiSettingsActivity() {
+    override fun shouldShowCollections(): Boolean = false
+}
