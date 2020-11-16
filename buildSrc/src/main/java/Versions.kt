94840dbf5a832db99aa94f59d3cb63e458d46f8c
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-11-16 14:29:47 -0500

    Update

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,7 +2,7 @@
 
 object Versions {
     // Plugins
-    const val gradle = "4.1.0"
+    const val gradle = "4.1.1"
     const val kotlin = "1.4.10"
     const val jitpack = "2.1"
 
@@ -13,10 +13,10 @@ object Versions {
     // App
     const val minSdk = 21
     const val targetSdk = 30
-    const val buildTools = "29.0.3"
+    const val buildTools = "30.0.0"
 
     // Frames
-    const val frames = "3.3.4"
+    const val frames = "3.3.5"
 
     // Kustom API
     const val kustomApi = "6369c37"
