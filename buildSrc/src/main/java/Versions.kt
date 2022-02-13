c9a17a810bb124bfb113855dfee7e56124e93f2e
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2022-02-13 18:19:11 -0500

    Update

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,25 +2,25 @@
 
 object Versions {
     // Plugins
-    const val gradle = "7.0.3"
+    const val gradle = "7.1.1"
     const val kotlin = "1.5.31"
     const val sonatype = "1.1.0"
 
     // OneSignal
     const val oneSignalPlugin = "0.14.0"
-    const val oneSignal = "4.6.3"
+    const val oneSignal = "4.6.7"
 
     // App
     const val minSdk = 21
     const val targetSdk = 31
-    const val buildTools = "30.0.2"
+    const val buildTools = "30.0.3"
 
     // Frames
-    const val frames = "3.4.6"
+    const val frames = "3.4.8"
 
     // Kustom API
     const val kustomApi = "6369c37"
 
     // Lifecycle Runtime KTX
-    const val lifecycleRuntimeKtx = "2.4.0"
+    const val lifecycleRuntimeKtx = "2.4.1"
 }
