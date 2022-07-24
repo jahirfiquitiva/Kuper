bad07e3f3ab9376d9e432a1983a1a3ac1541f304
Patryk Michalik <contact@patrykmichalik.com>
2022-07-24 23:07:01 +0200

    Update to Frames 3.4.9

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,25 +2,26 @@
 
 object Versions {
     // Plugins
-    const val gradle = "7.1.1"
-    const val kotlin = "1.5.31"
+    const val gradle = "7.2.1"
+    const val kotlin = "1.7.0"
     const val sonatype = "1.1.0"
+    const val ksp = "$kotlin-1.0.6"
 
     // OneSignal
     const val oneSignalPlugin = "0.14.0"
-    const val oneSignal = "4.6.7"
+    const val oneSignal = "4.8.1"
 
     // App
     const val minSdk = 21
-    const val targetSdk = 31
-    const val buildTools = "30.0.3"
+    const val targetSdk = 32
+    const val buildTools = "32.0.0"
 
     // Frames
-    const val frames = "3.4.8"
+    const val frames = "3.4.9"
 
     // Kustom API
     const val kustomApi = "6369c37"
 
     // Lifecycle Runtime KTX
-    const val lifecycleRuntimeKtx = "2.4.1"
+    const val lifecycleRuntimeKtx = "2.5.0"
 }
