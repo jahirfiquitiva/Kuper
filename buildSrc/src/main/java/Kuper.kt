4a47722810cc75159b8d6364a19672bccc94deac
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-02-24 15:24:14 -0500

    Fix Kolorette showing as not installed

--- buildSrc/src/main/java/Kuper.kt
+++ buildSrc/src/main/java/Kuper.kt
@@ -2,6 +2,6 @@
 
 object Kuper {
     const val appId = "dev.jahir.kuper.app"
-    const val version = 242
-    const val versionName = "2.4.2"
+    const val version = 243
+    const val versionName = "2.4.3"
 }
