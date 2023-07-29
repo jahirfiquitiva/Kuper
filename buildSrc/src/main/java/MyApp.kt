efe74ec045a86a32954446a1e2d5cd0a72cb0012
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-07-29 11:26:55 -0500

    Update to Kuper 2.4.7

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "dev.jahir.kuper.app"
-    const val version = 246
-    const val versionName = "2.4.6"
+    const val version = 247
+    const val versionName = "2.4.7"
 }
