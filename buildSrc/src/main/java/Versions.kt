f6c12f94d76d4ee9613a2098ec245797c44c9629
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2024-04-20 20:50:40 -0500

    chore: update dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,13 +2,13 @@
 
 object Versions {
     // Plugins
-    const val gradle = "8.2.0"
-    const val kotlin = "1.9.22"
+    const val gradle = "8.3.0"
+    const val kotlin = "1.9.23"
     const val sonatype = "1.3.0"
-    const val ksp = "$kotlin-1.0.16"
+    const val ksp = "$kotlin-1.0.20"
 
     // OneSignal
-    const val oneSignal = "4.8.7"
+    const val oneSignal = "4.8.8"
 
     // App
     const val minSdk = 21
@@ -16,11 +16,11 @@ object Versions {
     const val buildTools = "34.0.0"
 
     // Frames
-    const val frames = "3.6.0"
+    const val frames = "3.6.1"
 
     // Kustom API
-    const val kustomApi = "20"
+    const val kustomApi = "21"
 
     // Lifecycle Runtime KTX
-    const val lifecycleRuntimeKtx = "2.6.2"
+    const val lifecycleRuntimeKtx = "2.7.0"
 }
