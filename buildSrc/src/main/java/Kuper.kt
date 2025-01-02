53a584d1025607ee883580cf00abb09cbc16ea19
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2025-01-02 10:41:08 -0500

    chore: update kuper version

--- buildSrc/src/main/java/Kuper.kt
+++ buildSrc/src/main/java/Kuper.kt
@@ -2,6 +2,6 @@
 
 object Kuper {
     const val appId = "dev.jahir.kuper.app"
-    const val version = 252
-    const val versionName = "2.5.2"
+    const val version = 253
+    const val versionName = "2.5.3"
 }
