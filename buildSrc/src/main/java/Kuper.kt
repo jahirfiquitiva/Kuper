50aee12015d5239fb7e844b076901e93423d01c3
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2025-08-16 18:40:32 -0500

    chore: update dependencies and publish process

--- buildSrc/src/main/java/Kuper.kt
+++ buildSrc/src/main/java/Kuper.kt
@@ -2,6 +2,6 @@
 
 object Kuper {
     const val appId = "dev.jahir.kuper.app"
-    const val version = 255
-    const val versionName = "2.5.5"
+    const val version = 256
+    const val versionName = "2.5.6"
 }
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,10 +2,10 @@
 
 object Versions {
     // Plugins
-    const val gradle = "8.10.0"
-    const val kotlin = "2.1.21"
+    const val gradle = "8.12.0"
+    const val kotlin = "2.2.0"
     const val sonatype = "2.0.0"
-    const val ksp = "$kotlin-2.0.1"
+    const val ksp = "$kotlin-2.0.2"
 
     // OneSignal
     const val oneSignal = "4.8.12"
@@ -16,11 +16,11 @@ object Versions {
     const val buildTools = "36.0.0"
 
     // Frames
-    const val frames = "3.6.5"
+    const val frames = "3.7.0"
 
     // Kustom API
     const val kustomApi = "21"
 
     // Lifecycle Runtime KTX
-    const val lifecycleRuntimeKtx = "2.9.1"
+    const val lifecycleRuntimeKtx = "2.9.2"
 }
